8b225f30d89d971b5ec3b62063687edbc34feb24
Covali Andrei <krab4ek@gmail.com>
2022-07-19 22:01:37 +0300

    Add project files.

---
+++ covali.CustomEnumeratorWithYield/Garage.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+
+namespace covali.CustomEnumeratorWithYield;
+
+public class Garage : IEnumerable
+{
+    private Car[] carArray = new Car[4];
+    public Garage()
+    {
+        carArray[0] = new Car("Rusty", 20);
+        carArray[1] = new Car("Clunker", 56);
+        carArray[2] = new Car("Andy", 45);
+        carArray[3] = new Car("Rsty", 29);
+
+    }
+
+    public object Current => throw new NotImplementedException();
+
+    public IEnumerator GetEnumerator()
+    {
+        foreach (Car c in carArray)
+            yield return c;
+    }
+
+}
---
+++ covali.CustomEnumeratorWithYield/Radio.cs
@@ -0,0 +1,6 @@
+﻿namespace covali.CustomEnumeratorWithYield;
+
+internal class Radio
+{
+    public void TurnOn(bool on) => Console.WriteLine(on ? "Jamming..." : "Quiet time...");
+}
